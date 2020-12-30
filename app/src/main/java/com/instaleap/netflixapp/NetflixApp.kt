3395f13b849bf7bce9bac965108bc956dc1a949a
DESKTOP-HSTQ9S3\Familia Quezada <Carlos>
2020-12-30 14:17:07 -0500

    Dependencias agreadas, implementacion de App

--- app/src/main/java/com/instaleap/netflixapp/NetflixApp.kt
+++ app/src/main/java/com/instaleap/netflixapp/NetflixApp.kt
@@ -1,4 +1,27 @@
 package com.instaleap.netflixapp
 
-class NetflixApp {
+import android.app.Application
+import com.instaleap.dato.di.dtoConverterModule
+import com.instaleap.dato.di.movieDataSourceModule
+import com.instaleap.dato.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.fragment.koin.fragmentFactory
+import org.koin.core.context.startKoin
+
+class NetflixApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@NetflixApp)
+            fragmentFactory()
+            modules(listOf(
+                dtoConverterModule,
+                movieDataSourceModule,
+                networkModule
+            ))
+        }
+    }
+
 }
