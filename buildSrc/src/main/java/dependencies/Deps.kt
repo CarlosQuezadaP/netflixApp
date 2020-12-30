4a291c0d4f17d00d7e6f835406c2fc0502d7757f
DESKTOP-HSTQ9S3\Familia Quezada <Carlos>
2020-12-29 22:57:05 -0500

    implementacion de configuracion para centralizar las dependencias

---
+++ buildSrc/src/main/java/dependencies/Deps.kt
@@ -0,0 +1,17 @@
+package dependencies
+
+object Deps {
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin_version}"
+    const val core_ktx = "androidx.core:core-ktx:${Versions.Core.core_ktx}"
+
+    const val appcompat = "androidx.appcompat:appcompat:${Versions.AndroidX.appcompat}"
+    const val constraintlayout =
+        "androidx.constraintlayout:constraintlayout:${Versions.AndroidX.constrainLayout}"
+
+    const val material = "com.google.android.material:material:${Versions.material}"
+
+    const val junit = "junit:junit:4.+:${Versions.Test.junit}}"
+    const val junit2 = "androidx.test.ext:junit:1.1.2:${Versions.Test.junit_test}"
+    const val espresso = "androidx.test.espresso:espresso-core:3.3.0:${Versions.Test.espresso}"
+}
+
---
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -0,0 +1,33 @@
+package dependencies
+
+object Versions {
+    const val kotlin_version = "1.4.21"
+    const val material = "1.2.1"
+
+    object Core {
+        const val core_ktx = "1.3.2"
+    }
+
+    object AndroidX {
+        const val appcompat = "1.2.0"
+        const val constrainLayout = "2.0.4"
+    }
+
+    object Test {
+        const val junit = "4.+"
+        const val junit_test = "1.1.2"
+        const val espresso = "3.3.0"
+    }
+
+
+    object AndroidV {
+        const val compileSdkVersion = 30
+        const val buildToolsVersion = "30.0.2"
+        const val minSdkVersion = 23
+        const val targetSdkVersion = 30
+        const val versionCode = 1
+        const val versionName = "1.0"
+    }
+
+
+}
