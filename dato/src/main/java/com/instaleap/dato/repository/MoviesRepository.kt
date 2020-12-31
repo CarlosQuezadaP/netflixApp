ab85acbd880730dd73503670eede49e75112b589
DESKTOP-HSTQ9S3\Familia Quezada <Carlos>
2020-12-31 09:32:32 -0500

    Implementacion en modulo repository

--- dato/src/main/java/com/instaleap/dato/repository/MoviesRepository.kt
+++ dato/src/main/java/com/instaleap/dato/repository/MoviesRepository.kt
@@ -1,7 +1,13 @@
 package com.instaleap.dato.repository
 
 import com.instaleap.dato.datasource.MovieDataSource
+import com.instaleap.domain.models.MovieItemDomain
 
-class MoviesRepository(private val movieDataSource: MovieDataSource) {
+class MoviesRepository(private val movieDataSource: MovieDataSource) :
+    com.instaleap.usecases.repository.IMovieRepository {
+
+    override suspend fun getPopularMovies(page: Int): List<MovieItemDomain> =
+        movieDataSource.getPopularMovies(page)
+
 
 }
