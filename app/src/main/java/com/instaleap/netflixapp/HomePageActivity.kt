d26c5da634bdd43a465c93a51fae46040312a11f
Carlos Quezada <carlos.quezada@ceiba.com.co>
2021-01-03 12:18:28 -0500

    Diseño basico
    Flujo basico de la aplicacion finalizado con navigation

--- app/src/main/java/com/instaleap/netflixapp/HomePageActivity.kt
+++ app/src/main/java/com/instaleap/netflixapp/HomePageActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.instaleap.netflixapp.databinding.ActivityHomePageBinding
 import com.instaleap.netflixapp.viewmodels.UseCasesViewModel
@@ -20,32 +18,13 @@ class HomePageActivity : AppCompatActivity() {
 
     private val navController: NavController by lazy { findNavController(R.id.fragment_nav_host) }
 
-    private val appBarConfiguration: AppBarConfiguration by lazy {
-        AppBarConfiguration.Builder(
-            setOf(
-                R.id.homeFragment,
-                R.id.fragmentMovies,
-                R.id.fragmentSeries,
-            )
-        ).build()
-    }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         homePageBinding = ActivityHomePageBinding.inflate(layoutInflater)
         setContentView(homePageBinding.root)
 
-        setSupportActionBar(tlb_main)
-        setupActionBarWithNavController(navController, appBarConfiguration)
         bottom_navigation.setupWithNavController(navController)
-    }
-
-
-    override fun onResume() {
-        super.onResume()
-        getMovies()
     }
 
     private fun getMovies() {
@@ -53,6 +32,13 @@ class HomePageActivity : AppCompatActivity() {
 
         })
     }
+
+    private fun getMoviesGenres() {
+        useCasesViewModel.movieGenresList.observe(this, {
+
+        })
+    }
+
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp()
