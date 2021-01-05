2cfc8aeeac26a727b45cebcdf9c8126804df03c1
Carlos Quezada <carlos.quezada@ceiba.com.co>
2021-01-04 23:26:49 -0500

    Interfaz repositorio modificada , metodos agregados

--- usecases/src/main/java/com/instaleap/usecases/repository/IMovieRepository.kt
+++ usecases/src/main/java/com/instaleap/usecases/repository/IMovieRepository.kt
@@ -1,7 +1,11 @@
 package com.instaleap.usecases.repository
 
+import com.instaleap.domain.models.GenreItemDomain
 import com.instaleap.domain.models.MovieItemDomain
+import com.instaleap.domain.models.TVDomain
 
 interface IMovieRepository {
     suspend fun getPopularMovies(page: Int): List<MovieItemDomain>
+    suspend fun getPopularTvs(page: Int): List<TVDomain>
+    suspend fun getGenresMovies(): List<GenreItemDomain>
 }
