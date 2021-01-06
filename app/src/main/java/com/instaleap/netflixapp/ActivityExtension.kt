8f4a7746871c8e345629ba5b679463c7e90b0d37
Carlos Quezada <carlos.quezada@ceiba.com.co>
2021-01-06 09:13:59 -0500

    extensiones agregadas

---
+++ app/src/main/java/com/instaleap/netflixapp/ActivityExtension.kt
@@ -0,0 +1,24 @@
+package com.instaleap.netflixapp
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.CollapsingToolbarLayout
+
+fun Fragment.applyToolbarMargin(toolbar: Toolbar) {
+    if (checkIsMaterialVersion()) {
+        toolbar.layoutParams =
+            (toolbar.layoutParams as CollapsingToolbarLayout.LayoutParams).apply {
+                topMargin = getStatusBarSize()
+            }
+    }
+}
+
+private fun Fragment.getStatusBarSize(): Int {
+    val idStatusBarHeight = resources.getIdentifier("status_bar_height", "dimen", "android")
+    return if (idStatusBarHeight > 0) {
+        resources.getDimensionPixelSize(idStatusBarHeight)
+    } else 0
+}
+
+
