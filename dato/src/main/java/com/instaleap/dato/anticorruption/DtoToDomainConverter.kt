353aee358ec4fdbaffd6b55114e7214a12d674a9
Carlos Quezada <carlos.quezada@ceiba.com.co>
2021-01-06 09:25:17 -0500

    dtos converter creados

--- dato/src/main/java/com/instaleap/dato/anticorruption/DtoToDomainConverter.kt
+++ dato/src/main/java/com/instaleap/dato/anticorruption/DtoToDomainConverter.kt
@@ -1,13 +1,15 @@
 package com.instaleap.dato.anticorruption
 
-import com.instaleap.domain.models.GenreItemDomain
-import com.instaleap.domain.models.MovieItemDomain
-import com.instaleap.domain.models.TVDomain
 import com.instaleap.dato.dto.GenreDto
 import com.instaleap.dato.dto.MovieDto
 import com.instaleap.dato.dto.TVDto
+import com.instaleap.dato.responses.movieDatailResponse.MovieDetailDto
+import com.instaleap.domain.models.GenreItemDomain
+import com.instaleap.domain.models.MovieDetailDomain
+import com.instaleap.domain.models.MovieItemDomain
+import com.instaleap.domain.models.TVDomain
 
-class DtoToDomainConverter() : IDtoToDomainConverter {
+class DtoToDomainConverter : IDtoToDomainConverter {
 
     override fun convertDtoToDomain(movieDto: MovieDto) =
         MovieItemDomain(
@@ -17,7 +19,7 @@ class DtoToDomainConverter() : IDtoToDomainConverter {
             isVideo = movieDto.video,
             title = movieDto.title,
             popularity = movieDto.popularity,
-            posterPath = movieDto.poster_path,
+            posterPath = movieDto.poster_path.orEmpty(),
             originalLanguage = movieDto.original_language,
             originalTitle = movieDto.original_title.orEmpty(),
             genreIds = movieDto.genre_ids,
@@ -32,7 +34,7 @@ class DtoToDomainConverter() : IDtoToDomainConverter {
 
     override fun convertDtoToDomain(tVDto: TVDto) =
         TVDomain(
-            backdrop_path = tVDto.backdrop_path,
+            backdrop_path = tVDto.backdrop_path.orEmpty(),
             first_air_date = tVDto.first_air_date,
             genre_ids = tVDto.genre_ids,
             id = tVDto.id,
@@ -42,9 +44,40 @@ class DtoToDomainConverter() : IDtoToDomainConverter {
             original_name = tVDto.original_name,
             overview = tVDto.overview,
             popularity = tVDto.popularity,
-            poster_path = tVDto.poster_path,
+            poster_path = tVDto.poster_path.orEmpty(),
             vote_average = tVDto.vote_average,
             vote_count = tVDto.vote_count
         )
+
+    override fun convertDtoToDomain(detailDto: MovieDetailDto): MovieDetailDomain {
+        val genres = detailDto.genres.map {
+            convertDtoToDomain(it)
+        }
+
+        return MovieDetailDomain(
+            adult = detailDto.adult,
+            backdrop_path = detailDto.backdrop_path,
+            budget = detailDto.budget,
+            genres = genres,
+            homepage = detailDto.homepage,
+            id = detailDto.id,
+            imdb_id = detailDto.imdb_id,
+            original_language = detailDto.original_language,
+            original_title = detailDto.original_title,
+            overview = detailDto.overview,
+            popularity = detailDto.popularity,
+            poster_path = detailDto.poster_path,
+            release_date = detailDto.release_date,
+            revenue = detailDto.revenue,
+            runtime = detailDto.runtime,
+            status = detailDto.status,
+            tagline = detailDto.tagline,
+            title = detailDto.title,
+            video = detailDto.video,
+            vote_average = detailDto.vote_average,
+            vote_count = detailDto.vote_count
+        )
+    }
+
 
 }
--- dato/src/main/java/com/instaleap/dato/anticorruption/IDtoToDomainConverter.kt
+++ dato/src/main/java/com/instaleap/dato/anticorruption/IDtoToDomainConverter.kt
@@ -1,14 +1,17 @@
 package com.instaleap.dato.anticorruption
 
-import com.instaleap.domain.models.GenreItemDomain
-import com.instaleap.domain.models.MovieItemDomain
-import com.instaleap.domain.models.TVDomain
 import com.instaleap.dato.dto.GenreDto
 import com.instaleap.dato.dto.MovieDto
 import com.instaleap.dato.dto.TVDto
+import com.instaleap.dato.responses.movieDatailResponse.MovieDetailDto
+import com.instaleap.domain.models.GenreItemDomain
+import com.instaleap.domain.models.MovieDetailDomain
+import com.instaleap.domain.models.MovieItemDomain
+import com.instaleap.domain.models.TVDomain
 
 interface IDtoToDomainConverter {
     fun convertDtoToDomain(movieDto: MovieDto): MovieItemDomain
     fun convertDtoToDomain(genreDto: GenreDto): GenreItemDomain
     fun convertDtoToDomain(tVDto: TVDto): TVDomain
+    fun convertDtoToDomain(detailDto: MovieDetailDto): MovieDetailDomain
 }
