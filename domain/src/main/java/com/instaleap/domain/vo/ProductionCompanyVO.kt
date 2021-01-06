b222253c46f201dace78f7a45ff343447c703f89
Carlos Quezada <carlos.quezada@ceiba.com.co>
2021-01-06 09:24:14 -0500

    Modelos de dominio agregados

---
+++ domain/src/main/java/com/instaleap/domain/vo/ProductionCompanyVO.kt
@@ -0,0 +1,8 @@
+package com.instaleap.domain.vo
+
+data class ProductionCompanyVO(
+    val id: Int,
+    val logo_path: String,
+    val name: String,
+    val origin_country: String
+)
---
+++ domain/src/main/java/com/instaleap/domain/vo/SpokenLanguageVO.kt
@@ -0,0 +1,7 @@
+package com.instaleap.domain.vo
+
+data class SpokenLanguageVO(
+    val english_name: String,
+    val iso_639_1: String,
+    val name: String
+)
