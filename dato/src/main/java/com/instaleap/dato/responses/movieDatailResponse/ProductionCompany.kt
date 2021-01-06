82c4f67eaee5d6edb38624e50772087e7eec39b0
Carlos Quezada <carlos.quezada@ceiba.com.co>
2021-01-06 09:24:56 -0500

    MovieDetailResponse modelos creados

---
+++ dato/src/main/java/com/instaleap/dato/responses/movieDatailResponse/ProductionCompany.kt
@@ -0,0 +1,8 @@
+package com.instaleap.dato.responses.movieDatailResponse
+
+data class ProductionCompany(
+    val id: Int,
+    val logo_path: String,
+    val name: String,
+    val origin_country: String
+)
---
+++ dato/src/main/java/com/instaleap/dato/responses/movieDatailResponse/ProductionCountry.kt
@@ -0,0 +1,6 @@
+package com.instaleap.dato.responses.movieDatailResponse
+
+data class ProductionCountry(
+    val iso_3166_1: String,
+    val name: String
+)
