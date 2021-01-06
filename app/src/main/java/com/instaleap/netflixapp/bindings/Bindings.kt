b4f6844c50cfb2977096506c0ad1f38e07d66d64
Carlos Quezada <carlos.quezada@ceiba.com.co>
2021-01-06 09:22:18 -0500

    Bindings agergados

--- app/src/main/java/com/instaleap/netflixapp/bindings/Bindings.kt
+++ app/src/main/java/com/instaleap/netflixapp/bindings/Bindings.kt
@@ -1,10 +1,15 @@
 package com.instaleap.netflixapp.bindings
 
+import android.annotation.SuppressLint
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.instaleap.domain.models.MovieDetailDomain
+import com.instaleap.netflixapp.Api
 import com.instaleap.netflixapp.BuildConfig
+import com.instaleap.netflixapp.requestGlideListener
 
 
 @BindingAdapter("circleImage")
@@ -27,3 +32,25 @@ fun loadImage(view: ImageView, imageUrl: String?) {
         .load(url).apply(RequestOptions())
         .into(view)
 }
+
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("bindReleaseDate")
+fun bindReleaseDate(view: TextView, releaseDate: MovieDetailDomain?) {
+    view.text = "Release Date : ${releaseDate?.release_date}"
+}
+
+
+@BindingAdapter("bindBackDrop")
+fun bindBackDrop(view: ImageView, movie: MovieDetailDomain?) {
+    if (movie?.backdrop_path != null) {
+        Glide.with(view.context).load(movie?.backdrop_path?.let { Api.getBackdropPath(it) })
+            .listener(view.requestGlideListener())
+            .into(view)
+    } else {
+        Glide.with(view.context).load(movie?.poster_path?.let { Api.getBackdropPath(it) })
+            .listener(view.requestGlideListener())
+            .into(view)
+    }
+}
+
